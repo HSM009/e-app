a0eb5ee73e65fee051a4d33ad8e2c1e862f8a88e
HSM009 <hoseinsirat@gmail.com>
2023-07-21 10:53:58 +0500

    fottering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "./sections/body components/Header";
+import { Footer } from "./sections/body components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {<Header />}
         {children}
+        {<Footer />}
       </body>
     </html>
   );
